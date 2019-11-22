21ea8311c82b6841bd287d6e9ae3c30ff06ee71b
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-11-22 12:24:25 +0100

    FIX #4 added dos2unix conversion

--- scripts/bootstrapservices.sh
+++ scripts/bootstrapservices.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-
+set -x
+yum -q -y install dos2unix
+dos2unix /vagrant/scripts/jupyterinapache.sh
 /vagrant/scripts/jupyterinapache.sh add vagrant 8901 /jupyter /var/log/jupyter.log
