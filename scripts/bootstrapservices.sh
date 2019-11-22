50621d65673bb1265b0993d3fc0108c79633999f
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-11-22 12:30:16 +0100

    FIX #5 added pyfmi

--- scripts/bootstrapservices.sh
+++ scripts/bootstrapservices.sh
@@ -3,3 +3,10 @@ set -x
 yum -q -y install dos2unix
 dos2unix /vagrant/scripts/jupyterinapache.sh
 /vagrant/scripts/jupyterinapache.sh add vagrant 8901 /jupyter /var/log/jupyter.log
+# install pyfmi
+# the rest as vagrant
+tail -n +$[LINENO+2] $0 | exec sudo -u vagrant bash                                                                                                                                                                                                     
+exit $? 
+# install pyfmi
+conda install -y -c conda-forge pyfmi
+exit 0
