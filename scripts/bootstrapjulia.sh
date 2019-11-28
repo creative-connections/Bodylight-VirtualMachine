8383dcdd6c5f6ca022bfb4aefb345ede3af005f6
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-11-28 13:37:22 +0100

    ADD: cache julia tar

--- scripts/bootstrapjulia.sh
+++ scripts/bootstrapjulia.sh
@@ -3,9 +3,12 @@ set -x
 
 #sudo in case this script is executed after installation
 sudo yum install -y wget bzip2
-wget --quiet https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.0-linux-x86_64.tar.gz -O /home/vagrant/julia.tar.gz
+if [ ! -f /vagrant/julia.tar.gz ]; then
+  echo downloading julia
+  wget --quiet https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.0-linux-x86_64.tar.gz -O /vagrant/julia.tar.gz
+fi
 cd /home/vagrant
-tar -xzf /home/vagrant/julia.tar.gz
+tar -xzf /vagrant/julia.tar.gz
 /home/vagrant/julia-1.3.0/bin/julia -e "using Pkg; Pkg.add(\"IJulia\")"
 systemctl stop jupyter
 systemctl start jupyter
