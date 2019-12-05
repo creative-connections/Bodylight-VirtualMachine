f64b4ec5487f63f54bac5e7f9dac3d451db71e57
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-12-05 13:43:11 +0100

    update

--- scripts/bootstrapservices.sh
+++ scripts/bootstrapservices.sh
@@ -10,7 +10,16 @@ tail -n +$[LINENO+2] $0 | exec sudo -u vagrant bash
 exit $? 
 # install pyfmi and mamba
 /home/vagrant/jupyter/bin/conda install -q -y -c conda-forge pyfmi mamba
-/home/vagrant/jupyter/bin/mamba install -q -y -c conda-forge sos sos-pbs sos-notebook jupyterlab-sos sos-bash sos-python
+
+n=0
+until [ $n -ge 5 ]
+do
+   echo "attempting to do mamba install "$n
+   /home/vagrant/jupyter/bin/mamba install -q -y -c conda-forge sos sos-pbs sos-notebook jupyterlab-sos sos-bash sos-python && break  # substitute your command here
+   n=$[$n+1]
+   sleep 15
+done
+
 # install julia dependencies for jupyter (ijulia) and demo notebook using rdatasets
 /home/vagrant/julia-1.3.0/bin/julia -e "using Pkg; Pkg.add(\"IJulia\");Pkg.add(\"RDatasets\");Pkg.add(\"Gadfly\")"
 sudo systemctl stop jupyter
