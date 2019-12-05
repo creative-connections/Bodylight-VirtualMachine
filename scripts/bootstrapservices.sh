e3c4a00965fed65997516b4f3281523e3d800c56
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-12-05 12:24:35 +0100

    UPDATE: mamba install sos

--- scripts/bootstrapservices.sh
+++ scripts/bootstrapservices.sh
@@ -9,8 +9,7 @@ tail -n +$[LINENO+2] $0 | exec sudo -u vagrant bash
 exit $? 
 # install pyfmi and mamba
 /home/vagrant/jupyter/bin/conda install -q -y -c conda-forge pyfmi mamba
-/home/vagrant/jupyter/bin/conda activate
-mamba install sos sos-pbs sos-notebook jupyterlab-sos sos-bash sos-python -c conda-forge
+/home/vagrant/jupyter/bin/mamba install sos sos-pbs sos-notebook jupyterlab-sos sos-bash sos-python -c conda-forge
 # install julia dependencies for jupyter (ijulia) and demo notebook using rdatasets
 /home/vagrant/julia-1.3.0/bin/julia -e "using Pkg; Pkg.add(\"IJulia\");Pkg.add(\"RDatasets\");Pkg.add(\"Gadfly\")"
 sudo systemctl stop jupyter
