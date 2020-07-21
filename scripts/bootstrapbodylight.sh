b1bd8c1f0215ba85f59e1d681ade1475317c06c5
Tomas Kulhanek <tomaton@centrum.cz>
2020-07-21 11:57:53 +0200

    ADD compiler service and web app

--- scripts/bootstrapbodylight.sh
+++ scripts/bootstrapbodylight.sh
@@ -6,7 +6,7 @@ yum -y install git
 #curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 curl -sL https://rpm.nodesource.com/setup_14.x | bash -
 yum -y remove nodejs
-yum -y install nodejs 
+yum -y install nodejs
 
   cat <<EOF > /etc/httpd/conf.d/bodylight.conf
 Alias "/composer" "/home/vagrant/Bodylight.js-Composer/dist/"
@@ -15,7 +15,7 @@ Alias "/composer" "/home/vagrant/Bodylight.js-Composer/dist/"
   Require all granted
   Options FollowSymLinks IncludesNOEXEC
   AllowOverride All
-</Directory> 
+</Directory>
 
 Alias "/scenarios" "/home/vagrant/Bodylight-Scenarios/"
 <Directory "/home/vagrant/Bodylight-Scenarios">
@@ -23,7 +23,7 @@ Alias "/scenarios" "/home/vagrant/Bodylight-Scenarios/"
   Require all granted
   Options FollowSymLinks IncludesNOEXEC
   AllowOverride All
-</Directory> 
+</Directory>
 
 Alias "/virtualbody" "/home/vagrant/Bodylight-VirtualBody/dist/"
 <Directory "/home/vagrant/Bodylight-VirtualBody/dist">
@@ -42,6 +42,17 @@ Alias "/components" "/home/vagrant/Bodylight.js-Components/webcomponents/dist/"
   AllowOverride All
 </Directory>
 
+Alias "/compiler" "/home/vagrant/Bodylight.js-FMU-Compiler/"
+<Directory "/home/vagrant/Bodylight.js-FMU-Compiler">
+  Options +ExecCGI
+  AddHandler cgi-script .py
+  Header set Access-Control-Allow-Origin "*"
+  Require all granted
+  Options +Indexes +FollowSymLinks +IncludesNOEXEC
+  IndexOptions FancyIndexing HTMLTable NameWidth=*
+  AllowOverride All
+</Directory>
+
 EOF
 service httpd reload
 
@@ -52,6 +63,8 @@ cat <<EOF >>/var/www/html/index.html
 <a href="/virtualbody/"><div><u>Virtual Body App</u><ul><li><u>/virtualbody/</u></li><li class="small">installed at <code>/home/vagrant/Bodylight-Scenarios</code></li></ul></div></a>
 <a href="/components/"><div><u>Web Components</u><ul><li><u>/components/</u></li><li class="small">installed at <code>/home/vagrant/Bodylight.js-Components</code></li></ul></div></a>
 <a href="/scenarios/"><div><u>Scenarios</u><ul><li><u>/scenarios/</u></li><li class="small">installed at <code>/home/vagrant/Bodylight-Scenarios</code></li></ul></div></a>
+<a href="/compiler/"><div><u>Bodylight.js Compiler</u><ul><li><u>/compiler/</u></li><li class="small">installed at <code>/home/vagrant/Bodylight.js-FMU-Compiler</code></li></ul></div></a>
+
 </body>
 </html>
 EOF
@@ -62,8 +75,8 @@ systemctl enable docker
 systemctl start docker
 
 # the rest as vagrant
-tail -n +$[LINENO+2] $0 | exec sudo -u vagrant bash                                                                                                                                                                                                     
-exit $? 
+tail -n +$[LINENO+2] $0 | exec sudo -u vagrant bash
+exit $?
 # install dev dependencies
 sudo npm install aurelia-cli -g
 
@@ -80,6 +93,28 @@ cd /home/vagrant
 git clone https://github.com/creative-connections/Bodylight.js-FMU-Compiler.git
 cd Bodylight.js-FMU-Compiler
 sudo docker build -t bodylight.js.fmu.compiler "$(pwd)"
+echo setting bodylight-compiler service
+cat <<EOF >>/etc/systemd/system/bodylight-compiler.service
+[Unit]
+Description=Bodylight Compiler Service
+
+[Service]
+Type=simple
+PIDFile=/var/run/bodylight-compiler-service.pid
+User=vagrant
+ExecStart=/home/vagrant/Bodylight.js-FMU-Compiler/run.sh
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=bodylight-compiler
+WorkingDirectory=/home/vagrant/Bodylight.js-FMU-Compiler
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#sudo -u vagrant $INSTALLDIR/bin/jupyter notebook --port $3 --no-browser &
+systemctl enable bodylight-compiler
+systemctl start bodylight-compiler
+
 # run docker compiler reads from /input - puts to /output
 # sudo docker run -d --name bodylight.js.fmu.compiler   -v $(pwd)/input:/input -v $(pwd)/output:/output --rm bodylight.js.fmu.compiler:latest bash worker.sh
 
@@ -90,7 +125,7 @@ git clone https://github.com/creative-connections/Bodylight-Scenarios.git
 # VirtualBody
 git clone https://github.com/creative-connections/Bodylight-VirtualBody.git
 cd Bodylight-VirtualBody
-# cache gltf files used in 
+# cache gltf files used in
 mkdir -p static/models
 python cachemodels.py
 # build 3D Virtualbody app
